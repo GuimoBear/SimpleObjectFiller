a4396339954e45aed21bc07dd23b2a53b4b71a0a
Guilherme Barros <guilherme@tecsoft.info>
2019-01-24 13:53:58 -0300

    Creation of bool generator and the necessary roles

---
+++ SimpleObjectFiller/Generators/Contracts/ITrueWeightBoolGeneratorRole.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleObjectFiller.Generators.Contracts
+{
+    /// <summary>
+    /// this role represents the probability(greater than or equal to 0.0 and less than 1.0) of chance of the generator returns a true value
+    /// </summary>
+    public interface ITrueWeightBoolGeneratorRole
+    {
+        double? TrueWeight { get; set; }
+    }
+}
---
+++ SimpleObjectFiller/Generators/Primitives/BoolGenerator.cs
@@ -0,0 +1,19 @@
+﻿using SimpleObjectFiller.Generators.Contracts;
+
+namespace SimpleObjectFiller.Generators.Primitives
+{
+
+    public class BoolGenerator : BaseGenerator<bool>, ITrueWeightBoolGeneratorRole
+    {
+        public double? TrueWeight { get; set; }
+
+        protected override bool Generate()
+        {
+            if (hasDefaultValue)
+                return DefaultValue;
+            if(TrueWeight.HasValue)
+                return random.NextDouble() <= TrueWeight.Value;
+            return random.Next() % 2 == 0;
+        }
+    }
+}
