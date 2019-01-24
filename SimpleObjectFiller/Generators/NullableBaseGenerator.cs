3d037240bb460d02f012fea08c76fee7dcdfeefa
Guilherme Barros <guilherme@tecsoft.info>
2019-01-24 13:55:19 -0300

    Creation of the nullable base generator and the role of null weight generation

---
+++ SimpleObjectFiller/Generators/NullableBaseGenerator.cs
@@ -0,0 +1,22 @@
+﻿using SimpleObjectFiller.Generators.Contracts;
+using System;
+
+namespace SimpleObjectFiller.Generators
+{
+    public abstract class NullableBaseGenerator<T> : BaseGenerator<T?>, INullWeightGeneratorRole
+        where T : struct
+    {
+        private double? nullWeight;
+
+        public virtual double? NullWeight
+        {
+            get => nullWeight;
+            set
+            {
+                if (value.HasValue && (value.Value < 0 || value.Value >= 1))
+                    throw new ArgumentException("The NullWeight must be greater than or equal to 0 and less than 1");
+                nullWeight = value;
+            }
+        }
+    }
+}
